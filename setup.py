ca47cfe324b3b3f955d93f416c5891e729384f72
chris-c-mcintyre <chris-c-mcintyre@outlook.com>
2023-04-21 21:55:50 -0400

    Placeholder info for setup.py.

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name="rous",
+    version="0.1.0",
+    author="chris-c-mcintyre",
+    author_email="chris-c-mcintyre@outlook.com",
+    url="https://github.com/chris-c-mcintyre/rous",
+    description="Doing things the hard way.",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[],
+)
+
