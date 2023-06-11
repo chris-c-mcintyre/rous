93c22f8387e5a402da2a1631bae99b84f8a480c9
chris-c-mcintyre <chris-c-mcintyre@outlook.com>
2023-06-11 19:51:42 -0400

    Move document setup functions to display API.

--- index.js
+++ index.js
@@ -1,40 +1,8 @@
 /* module imports */
 
-import { generateDisplayObject, renderDisplayObject } from "./src/rous/api/display.js";
+import { generateDocumentTitle, generateDocumentFavicon, generateDisplayObject, renderDisplayObject } from "./src/rous/api/display.js";
 
 import { setupFileUpload } from "./src/rous/api/files.js";
-
-/* setup functions */
-
-function generateDocumentTitle( jsArgs, jsKwargs )
-{
-  document.head.appendChild
-  (
-    Object.assign
-    (
-      document.createElement("title"),
-      {
-        innerText: jsKwargs.text + jsKwargs.space + jsKwargs.icon
-      }
-    )
-  );
-}
-
-function generateDocumentFavicon( jsArgs, jsKwargs )
-{
-  // https://stackoverflow.com/questions/64452685/how-to-convert-unicode-character-to-svg-and-then-a-favicon
-
-  document.head.appendChild
-  (
-    Object.assign
-    (
-      document.createElement("link"),
-      {
-        rel: "icon",
-        href: `data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><text y='.9em' font-size='90'>${jsKwargs.icon}</text></svg>`}
-    )
-  );
-}
 
 /* program */
 
--- src/rous/api/display.js
+++ src/rous/api/display.js
@@ -1,4 +1,36 @@
 
+/* setup functions */
+
+function generateDocumentTitle( jsArgs, jsKwargs )
+{
+  document.head.appendChild
+  (
+    Object.assign
+    (
+      document.createElement("title"),
+      {
+        innerText: jsKwargs.text + jsKwargs.space + jsKwargs.icon
+      }
+    )
+  );
+}
+
+function generateDocumentFavicon( jsArgs, jsKwargs )
+{
+  // https://stackoverflow.com/questions/64452685/how-to-convert-unicode-character-to-svg-and-then-a-favicon
+
+  document.head.appendChild
+  (
+    Object.assign
+    (
+      document.createElement("link"),
+      {
+        rel: "icon",
+        href: `data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><text y='.9em' font-size='90'>${jsKwargs.icon}</text></svg>`}
+    )
+  );
+}
+
 /* display functions */
 
 function generateDisplayLayer( jsArgs, jsKwargs )
@@ -57,5 +89,5 @@ function renderDisplayObject( jsArgs, jsKwargs )
   document.body.appendChild(displayDiv);
 }
 
-export { generateDisplayLayer, generateDisplayObject, renderDisplayObject };
+export { generateDocumentTitle, generateDocumentFavicon, generateDisplayLayer, generateDisplayObject, renderDisplayObject };
 
