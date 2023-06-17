e0d65c80fe7d8e4570b3687b749a14572ee9bca4
chris-c-mcintyre <chris-c-mcintyre@outlook.com>
2023-06-17 18:03:36 -0400

    Add WODE test cases for different OPTED byte start positions.

--- test/test_wode.py
+++ test/test_wode.py
@@ -19,6 +19,7 @@ class TestWodeMethods(unittest.TestCase):
 
     self.valid_word = "Parafatalism"
     self.valid_defn = "Deterministic when convenient."
+    self.opted_test_defn = "The principles or measures of abolitionists."
     self.correct_pair = (self.valid_word, self.valid_defn)
     self.year_of_seconds = 60 * 60 * 24 * 365 # ~31536000
     self.the_beginning = 0 # Universe begins on Thursday, January 1, 1970, at 12:00:00 AM UTC
@@ -52,6 +53,12 @@ class TestWodeMethods(unittest.TestCase):
 
   def test_opted_defn_len(self):
     self.assertTrue(len(word(use_opted=True)[1]) > 0)
+
+  def test_opted_start(self):
+    self.assertEqual(word(use_opted=True, start_type="start")[1], self.opted_test_defn)
+
+  def test_opted_end(self):
+    self.assertEqual(word(use_opted=True, start_type="end")[1], self.opted_test_defn)
 
 if __name__ == '__main__':
   unittest.main()
